b85ff67c20fb5191913ed5d6f72a917a2c58d19b
sgate-rahul <sgate.rahul@gmail.com>
2025-06-02 21:40:43 +0530

    add

--- imdb-clone/src/components/Banner.jsx
+++ imdb-clone/src/components/Banner.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import banner from "../assets/avengersd.jpg"
 function Banner() {
     return (
-        <div className='h-[20vh] md:h[70vh]' style={{ backgroundImage: `url(${banner})` }}>Banner</div>
+       <div className='h-[70vh] md:h-[110vh] bg-cover bg-center  flex items-end' style={{ backgroundImage: `url(https://images.hdqwalls.com/wallpapers/poster-avengers-endgame-ni.jpg)` }}>
+</div>
+
     )
 }
 
